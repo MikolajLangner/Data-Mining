27c819ff9c341482aadea9ff06410dcc14e5bad2
M.K <Manik24901@gmail.com>
2021-04-30 02:55:06 +0200

    Closer to the end

--- lista_2/3d_plots.R
+++ lista_2/3d_plots.R
@@ -1,23 +1,13 @@
 library(datasets)
 library(dplyr)
-library(MASS)
 library(cluster)
-library(DataExplorer)
-library(ggplot2)
-library(ggbeeswarm)
-library(ggfortify)
-library(ggbiplot)
-library(ggcorrplot)
 library(wesanderson)
-library(cowplot)
-library(EnvStats)
-library(arules)
-library(e1071)
-library(kableExtra)
-library(reshape2)
+library(RColorBrewer)
 library(rgl)
 library(car)
 
+
+# Zadanie 2 - PCA w 3D
 
 data(state)
 state <- as.data.frame(state.x77)
@@ -25,12 +15,29 @@ state$region <- state.region
 state$division <- state.division
 state.subset <- subset(state, select=-c(region, division))
 
-
 after.pca <- prcomp(state.subset, retx=T, center=T, scale.=T)
 
+scatter3d(x = after.pca$x[, 1], y = after.pca$x[, 2], z = after.pca$x[,3],
+          groups = state$region, surface = FALSE, ellipsoid = TRUE, 
+          xlab = 'PC1', ylab = 'PC2', zlab = 'PC3',
+          surface.col = wes_palette("Darjeeling1")[1:4])
 
-scatter3d(x=after.pca$x[, 1], y=after.pca$x[, 2], z=after.pca$x[,3], surface=FALSE, 
-          groups = as.factor(state$region), ellipsoid = TRUE)
-s3d <- scatter3d(x=after.pca$x[, 1], y=after.pca$x[, 2], z=after.pca$x[,3], surface=FALSE, 
-                groups = as.factor(state$region), ellipsoid = TRUE)
-text(s3d$xyz.convert(after.pca$x[, 1:3]), labels = rownames(state))
+
+# Zadanie 3 - MDS w 3D
+
+stars <- read.csv("Stars.csv")
+
+stars <- sample_n(read.csv("Stars.csv"), 100)
+stars$Color <- as.factor(stars$Color)
+stars$Spectral_Class <- as.factor(stars$Spectral_Class)
+data.for.mds <- subset(stars, select=-Type)
+
+dist.matrix <- as.matrix(daisy(data.for.mds))
+
+mds <- as.matrix(cmdscale(dist.matrix, k = 3))
+
+
+scatter3d(x=mds[, 1], y=mds[, 2], z=mds[,3], surface=FALSE, 
+          groups = as.factor(stars$Type), ellipsoid = TRUE, 
+          xlab = "MDS1", ylab = "MDS2", zlab = "MDS3", 
+          labels = parse0("", 1:100))
